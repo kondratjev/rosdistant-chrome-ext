2f181ee514d8564c3ee2aa52e7adadd03f79ea70
Max Kondratyev <kondratj3v@gmail.com>
2023-04-06 00:31:19 +0300

    Replace logo

--- src/manifest.ts
+++ src/manifest.ts
@@ -7,13 +7,11 @@ export default defineManifest({
   manifest_version: 3,
   icons: {
     '16': 'img/logo-16.png',
-    '32': 'img/logo-34.png',
-    '48': 'img/logo-48.png',
-    '128': 'img/logo-128.png',
+    '32': 'img/logo-32.png',
   },
   action: {
     default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_icon: 'img/logo-32.png',
   },
   options_page: 'options.html',
   background: {
@@ -31,9 +29,7 @@ export default defineManifest({
     {
       resources: [
         'img/logo-16.png',
-        'img/logo-34.png',
-        'img/logo-48.png',
-        'img/logo-128.png',
+        'img/logo-32.png',
         'src/content/index.css',
       ],
       matches: [],
